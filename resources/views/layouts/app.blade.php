f6b9461c7f089601861277a3380f5a0c38876c53
Andrei <fatandrei57@gmail.com>
2023-10-06 22:28:45 +0300

    Responsive Changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Funded Trader Money Management App') }}</title>
     <link rel="icon" type="image/x-icon" href={{asset('/assets/favicon.png')}}>
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
