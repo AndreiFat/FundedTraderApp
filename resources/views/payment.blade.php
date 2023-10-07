dbfb0eea231ed5fcfe5e035e91f9e8e23da4f1db
Andrei <fatandrei57@gmail.com>
2023-10-07 22:56:37 +0300

    payments page update

--- resources/views/payment.blade.php
+++ resources/views/payment.blade.php
@@ -4,9 +4,9 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <div class="card my-4 p-4">
+                <div class="card my-4 p-3">
                     <div class="card-body">
-                        <div class="">{{ __('Payment') }}</div>
+                        <div class="fs-2">{{ __('Payment') }}</div>
                         <p class="fs-5">One-Time Payment of $6.99 For Use on Desktop or Smartphone</p>
                         <a class="fs-5 btn btn-primary" href="">BUY NOW</a>
                         @error('error') is-invalid @enderror
